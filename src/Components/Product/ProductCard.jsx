63b98e1f26ed3a1b6b51b81097d72d811ef83892
Buska-bestejerba <lemu2005@yahoo.com>
2025-09-19 16:46:00 -0500

    update cart

--- src/Components/Product/ProductCard.jsx
+++ src/Components/Product/ProductCard.jsx
@@ -1,15 +1,15 @@
-import  { useContext } from "react";
+import {useContext} from "react";
 import Rating from "@mui/material/Rating";
 import CuurencyFormat from "../CurrencyFormat/CurencyFormat";
 import classes from "./product.module.css";
-import { Link } from "react-router-dom";
-import { DataContext } from "../DataProvider/DataProvider";
-import { Type } from "../../Utility/action.type";
+import {Link} from "react-router-dom";
+import {DataContext} from "../DataProvider/DataProvider";
+import {Type} from "../../Utility/action.type";
 
-function ProductCard({ product, flex }) {
-  const { id, image, title, price, rating, description } = product;
-  const [state, dispatch] = useContext(DataContext);
-  console.log(state);
+function ProductCard({product, flex, renderDesc = true, renderAdd = true}) {
+  const {id, image, title, price, rating, description} = product;
+  const [, dispatch] = useContext(DataContext);
+
   const addToCart = () => {
     dispatch({
       type: Type.ADD_TO_BASKET,
@@ -30,7 +30,11 @@ function ProductCard({ product, flex }) {
         <img src={image} alt={title} className={classes.productImage} />
         <h3 className={classes.title}>{title}</h3>
       </Link>
-      <p className={classes.desc}>{description?.substring(0, 70)}...</p>
+
+      {/* render description only if renderDesc is true */}
+      {renderDesc && (
+        <p className={classes.desc}>{description?.substring(0, 70)}...</p>
+      )}
 
       <div className={classes.rating}>
         <Rating value={rating?.rate || 0} precision={0.1} readOnly />
@@ -38,13 +42,15 @@ function ProductCard({ product, flex }) {
       </div>
 
       <div className={classes.price}>
-        {" "}
         <CuurencyFormat amount={price} />
       </div>
 
-      <button className={classes.button} onClick={addToCart}>
-        Add to Cart
-      </button>
+      {/* render Add to Cart button only if renderAdd is true */}
+      {renderAdd && (
+        <button className={classes.button} onClick={addToCart}>
+          Add to Cart
+        </button>
+      )}
     </div>
   );
 }
