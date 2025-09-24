d6c2ed94b248dce4f92a9bef0c4fdb34535f8236
Buska-bestejerba <lemu2005@yahoo.com>
2025-09-23 22:59:35 -0500

    Update axios.js

--- src/API/axios.js
+++ src/API/axios.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   //for local server firebase function
   // baseURL: "http://127.0.0.1:5001/clone-f442e/us-central1/api",
   // deployed version of amazon server in render.com
-  baseURL: "https://amazon-api-2day.onrender.com/",
+  baseURL: "https://amazon-api-1-j1mc.onrender.com/",
 });
 export { axiosInstance };
 
