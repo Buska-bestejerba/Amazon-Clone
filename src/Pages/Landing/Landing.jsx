0e06d42b0706bd980e43f91b878827d8913338c0
Buska-bestejerba <lemu2005@yahoo.com>
2025-09-23 22:45:23 -0500

    Update Landing.jsx

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,16 +1,16 @@
 import Carousel from "../../Components/Carousel/CarouselEffect";
 import Category from "../../Components/Category/Category";
-import Layout from "../../Components/Layout/Layout";
+import LayOut from "../../Components/Layout/Layout";
 import Product from "../../Components/Product/Product";
 
 function Landing() {
   return (
     <>
-      <Layout />
+      <LayOut>
         <Carousel />
         <Category />
         <Product />
-      <Layout />
+      </ LayOut >
     </>
   );
 }
